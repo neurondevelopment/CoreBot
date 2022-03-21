92b51dff1af626151859d335ea6ed892b3e4cdb6
Cryptonized <thomas_creasey@hotmail.co.uk>
2022-03-21 22:02:58 +0000

    Fix meme system

--- commands/general/meme.js
+++ commands/general/meme.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require('discord.js');
-const api = require("imageapi.js");
-let { subreddits } = require('../../config.json').commands.memes
+const {subbey} = require('subbey');
+let { subreddits, allowNSFW, topPosts } = require('../../config.json').commands.memes
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
@@ -11,16 +11,13 @@ module.exports = {
     async execute(interaction) {
       subreddits = subreddits.split(',');
       let subreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
-      let img = await api(subreddit, false);
-      while(img === undefined) {
-        subreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
-        img = await api(subreddit, true);
-      }
+      const img = await subbey({sub: subreddit, max: 1, nfsw: allowNSFW, top: topPosts});
+
       const Embed = new MessageEmbed()
         .setTitle(`From r/${subreddit}`)
         .setURL(`https://reddit.com/r/${subreddit}`)
         .setColor("RANDOM")
-        .setImage(img);
+        .setImage(img[0].media);
 
       
       interaction.reply({embeds: [Embed]});
